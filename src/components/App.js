1f6861dfc0299d07cbc1286165cfc7e7b8503f16
Vivek Mehta <vivekisback111@gmail.com>
2018-01-26 16:06:31 +0530

    Initial Project Set-up

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h2>Hello to my First Component</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <App />,
+    document.getElementById('app')
+);
