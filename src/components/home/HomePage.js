5b423ffe7ce5a02be465dd1da490d423dd35784e
Vivek Mehta <vivekisback111@gmail.com>
2018-02-09 18:53:41 +0530

    File UPDATED

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,7 +7,7 @@ class HomePage extends React.Component {
             <div className="jumbotron">
                 <h1>Corporate Administration</h1>
                 <p>React, Redux and React-Router in ES6 for ultra responsive web apps.</p>
-                <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+                <Link to="about" className="btn btn-secondary btn-lg">Learn More</Link>
             </div>
         );
     }
